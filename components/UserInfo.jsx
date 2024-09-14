8914b5b8fbf21cb3feef9d98f0cbf4039eecb858
Md-FarhadHossain <freelancerfarhadhossain@gmail.com>
2024-09-15 02:19:00 +0600

    loading added with svg

--- components/UserInfo.jsx
+++ components/UserInfo.jsx
@@ -71,7 +71,7 @@ const UserInfo = () => {
                                         type="submit"
                                         className="w-full bg-[#e2136e]"
                                     >
-                                        <span className='relative left-1'>Order Now -</span> <Image className='w-12 relative right-2' src={Illustration} />
+                                        <span className='relative left-1'>{loading ? "Please Wait" : "Order Now"} -</span> {loading ? <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200"><circle fill="#FF156D" stroke="#FF156D" stroke-width="15" r="15" cx="40" cy="65"><animate attributeName="cy" calcMode="spline" dur="2" values="65;135;65;" keySplines=".5 0 .5 1;.5 0 .5 1" repeatCount="indefinite" begin="-.4"></animate></circle><circle fill="#FF156D" stroke="#FF156D" stroke-width="15" r="15" cx="100" cy="65"><animate attributeName="cy" calcMode="spline" dur="2" values="65;135;65;" keySplines=".5 0 .5 1;.5 0 .5 1" repeatCount="indefinite" begin="-.2"></animate></circle><circle fill="#FF156D" stroke="#FF156D" stroke-width="15" r="15" cx="160" cy="65"><animate attributeName="cy" calcMode="spline" dur="2" values="65;135;65;" keySplines=".5 0 .5 1;.5 0 .5 1" repeatCount="indefinite" begin="0"></animate></circle></svg> : <Image className='w-12 relative right-2' src={Illustration} />}
                                     </Button>
                                 </div>
 
